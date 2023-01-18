90cd970917308774c92ca8224704a71357b800bc
Dhananjay Kalaskar <kalaskard123@gmail.com>
2023-01-18 15:37:36 +0530

    Create conatct array

--- contact/src/components/App.js
+++ contact/src/components/App.js
@@ -5,11 +5,25 @@ import AddContact from "./AddContact"
 import ContactList from "./ContactList"
 
 function App() {
+  const contacts = [
+    {
+      id: "1",
+      "name":"Dhananjay",
+      email: "dkboss@gmail.com",
+
+    },
+    {
+      id: "2",
+      "name":"chaitanya",
+      email: "ckboss@gmail.com",
+
+    }
+  ];
   return (
     <div>
       <Header />
       <AddContact />
-      <ContactList />
+      <ContactList contacts={contacts} />
       
     </div>
   );
